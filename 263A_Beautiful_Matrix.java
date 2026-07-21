b20bd1a96f93f7ac324b9396f07dc150d63340d8
Tilakraj Singh Rajawat <tilakrajawat004@gmail.com>
2026-07-21 19:37:17 +0530

    Sync Codeforces: 263A - Beautiful Matrix [Java 21]

---
+++ 263A_Beautiful_Matrix.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                int x= sc.nextInt();
+                if(x==1){
+              System.out.println(Math.abs(i-3)+Math.abs(j-3));
+                return;
+     }           
+            }
+        }
+    }
+}
