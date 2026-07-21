cb84e85c9baeb12791770b98364b4834a4e04f16
Tilakraj Singh Rajawat <tilakrajawat004@gmail.com>
2026-07-21 19:36:01 +0530

    Sync Codeforces: 1742A - Sum [Java 21]

---
+++ 1742A_Sum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ 
+public class codeforce {
+    public static void main(String[] args) {
+ 
+ 
+Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        for(long i=1;i<=n;i++){
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            long c = sc.nextLong();
+            if(a==(b+c) || b==(c+a) || c==(a+b)) {
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+            
+        }
+    }
+}
