8f24ad6866d0cbdf764bc0dddb0e71a940174f4b
Tilakraj Singh Rajawat <tilakrajawat004@gmail.com>
2026-07-24 11:09:42 +0530

    Sync Codeforces: 1030A - In Search of an Easy Problem [Java 21]

---
+++ 1030A_In_Search_of_an_Easy_Problem.java
@@ -0,0 +1,15 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     int n=sc.nextInt();
+     int ct=0;
+     for(int i=0;i<n;i++){
+       int num=sc.nextInt();
+       if(num==1) ct++;
+     }
+     if(ct>=1) System.out.print("HARD");
+     else System.out.print("EASY");
+    }
+}
